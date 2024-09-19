7160236e7a35641a71fdf69be23c2c28c39f536a
Pouyani Armin <pouyaniarmin@gmail.com>
2024-09-19 15:45:57 +0330

    This class is responsible for summarizing text by extracting a limited number of sentences

--- app/core/TextSummraizer.php
+++ app/core/TextSummraizer.php
@@ -1,17 +1,36 @@
-<?php 
+<?php
 
 namespace App\Core;
 
-class TextSummraizer{
-    
-    function summarizeText($textArray,$sentenceLimit=5){
-        $fullText=implode(' ',$textArray);
-        $sentences=$this->splitSentences($fullText);
-        $summary=array_slice($sentences,0,$sentenceLimit);
-        return implode(' ',$summary);
+/**
+ * Class TextSummarizer
+ * 
+ * This class is responsible for summarizing text by extracting a limited number of sentences.
+ */
+class TextSummraizer
+{
+    /**
+     * Summarizes the given text array by extracting a specified number of sentences.
+     *
+     * @param array $textArray An array of text chunks to be summarized.
+     * @param int $sentenceLimit The maximum number of sentences to include in the summary.
+     * @return string The summarized text.
+     */
+    function summarizeText($textArray, $sentenceLimit = 5)
+    {
+        $fullText = implode(' ', $textArray);
+        $sentences = $this->splitSentences($fullText);
+        $summary = array_slice($sentences, 0, $sentenceLimit);
+        return implode(' ', $summary);
     }
-
-    private function splitSentences($text){
-        return preg_split('/(?<=[.!?])\s+(?=[A-Z])/',$text,-1,PREG_SPLIT_NO_EMPTY);
+    /**
+     * Splits the text into sentences based on punctuation and capital letters.
+     *
+     * @param string $text The text to be split into sentences.
+     * @return array An array of sentences.
+     */
+    private function splitSentences($text)
+    {
+        return preg_split('/(?<=[.!?])\s+(?=[A-Z])/', $text, -1, PREG_SPLIT_NO_EMPTY);
     }
 }
